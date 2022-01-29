97b55fec8e1b82b2eedfd71e45bb2f316a7ea168
kevinah <kevinah@stud.ntnu.no>
2022-01-29 12:53:14 +0100

    targeting system isweird.

--- Assets/Scrips/Character.cs
+++ Assets/Scrips/Character.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Character : MonoBehaviour
 {
@@ -15,11 +18,27 @@ public class Character : MonoBehaviour
     public GameObject target;
     private GameObject enemies;
 
+    public GameObject targeter_prefab;
+
     void make_choice()
     {
         rand_dmg = Random.Range(0, max_rand_dmg);
         target = enemies.transform.GetChild(Random.Range(0, enemies.transform.childCount)).gameObject;
         Debug.Log(gameObject.name + " targets " + target.name);
+        
+        Quaternion rot = Quaternion.identity;
+        if (!ally)
+        {
+            rot = Quaternion.Euler(0,180,0);
+        }
+        
+        float to_x = target.transform.position.x;
+        float from_x =  transform.position.x;
+        Instantiate(targeter_prefab,
+            new Vector3((from_x + to_x) / 2f, 2.0f, 0.0f),
+            rot);
+        Vector3 direction = target.transform.position - targeter_prefab.transform.position;
+        targeter_prefab.transform.localScale= new Vector3(direction.magnitude/2f,2f,1);
     }
     
     // Start is called before the first frame update
