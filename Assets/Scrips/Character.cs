ad7a59c44f36e03e5ccdaabd7662a34247a2c9d9
kevinah <kevinah@stud.ntnu.no>
2022-01-29 01:51:43 +0100

    Basic UI created, targeting and damage calc is rolled. Cards are animated. Hitbox of card is broke

--- Assets/Scrips/Character.cs
+++ Assets/Scrips/Character.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public bool ally;
+    
+    private int hp = 100;
+
+    public int base_dmg = 1;
+    public int max_rand_dmg = 4;
+    public int rand_dmg;
+
+    public GameObject target;
+    private GameObject enemies;
+
+    void make_choice()
+    {
+        rand_dmg = Random.Range(0, max_rand_dmg);
+        target = enemies.transform.GetChild(Random.Range(0, enemies.transform.childCount)).gameObject;
+        Debug.Log(gameObject.name + " targets " + target.name);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (ally)
+        {
+            enemies = GameObject.Find("Enemies");
+        }
+        else
+        {
+            enemies = GameObject.Find("Allies");
+        }
+        make_choice();
+        Debug.Log(gameObject.name + " with base "+base_dmg+" and rand addition " + rand_dmg + 
+                  "\nWill target "+enemies.name);
     }
-
+    
+    
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scrips/card.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class card : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Card created");
+    }
+    void OnMouseEnter()
+    {
+        print("Mouse hover over "+gameObject.name);
+        gameObject.transform.position += transform.up;
+
+    }
+    void OnMouseExit()
+    {
+        print("Mouse is no longer on card"+gameObject.name);
+        gameObject.transform.position -= transform.up;
+        
+    }
+}
